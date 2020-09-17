453f2e67596db409333aff31407bccefbff60d00
AngelikaMikolajczyk <67585301+AngelikaMikolajczyk@users.noreply.github.com>
2020-09-17 17:32:57 +0200

    change demo data

--- examples/simple-api/data/todos.js
+++ examples/simple-api/data/todos.js
@@ -1,6 +1,6 @@
 const todos = [
-	{id: 1, name: 'Do laundry!', isDone: false},
-	{id: 2, name: 'Be happy!', isDone: true},
+	{id: 1, name: 'Do laundry!!!', isDone: false},
+	{id: 2, name: 'Be happy!!!!!', isDone: true},
 	{id: 3, name: 'Make a great api!', isDone: true},
 ];
 
